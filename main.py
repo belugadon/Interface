831cd2856b3945d3c22cfdefb01ce0f9497d601d
Tristan <tristanb_44@hotmail.com>
2017-09-20 22:27:07 -0300

    added serial functions and file IO

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import serial
+import Tkinter
+
+file = open("flightdata.csv", 'w')
+file.write("Angular Position \n x:, y:, z:\n")
+
+
+ser = serial.Serial('COM6', 9600, timeout=1)  # open serial port
+print(ser.name)         # check which port was really used
+while 1:
+	x = ser.read() 
+	file.write(x)
+	
+ser.close()             # close port
