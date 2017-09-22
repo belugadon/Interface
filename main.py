7da5bc6cfdf52a3004562f56c50aff411ab15cf5
Tristan <tristanb_44@hotmail.com>
2017-09-21 21:59:40 -0300

    Added first components of The Graphical GUI Interface for users

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import serial
-import Tkinter
+from Tkinter import Tk, Label, Button
 
-file = open("flightdata.csv", 'w')
-file.write("Angular Position \n x:, y:, z:\n")
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Log Data", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        file = open("flightdata.csv", 'w')
+	file.write("Angular Position \n x:, y:, z:\n")
+	ser = serial.Serial('COM6', 9600, timeout=1)  # open serial port
+	print(ser.name)         # check which port was really used
+	while 1:
+		x = ser.read() 
+		file.write(x)
+		print(x)
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
 
 
-ser = serial.Serial('COM6', 9600, timeout=1)  # open serial port
-print(ser.name)         # check which port was really used
-while 1:
-	x = ser.read() 
-	file.write(x)
 	
 ser.close()             # close port
