9c79a25adc2741220be0fd9c3aa5282d1700df83
Tristan <tristanb_44@hotmail.com>
2017-09-23 14:01:24 -0300

    added beginning of graphic display

--- main.py
+++ main.py
@@ -28,46 +28,61 @@ class MyFirstGUI:
         self.start_button = Button(master, text="Log Data", command=self.start_background_task)
         self.start_button.place(x=0, y=96, width=80, height=20)
         
-        self.stop_button = Button(master, text="Stop Logging", command=stop_logging)
+        self.stop_button = Button(master, text="Stop Logging", command=self.stop_logging)
         self.stop_button.place(x=81, y=96, width=80, height=20)
 
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.grid(row=10, column=0, columnspan=1, sticky=S)
         self.close_button.place(x=0, y=116, width=60, height=20)
 
-        self.w = Canvas(master, width=200, height=100)
-        self.w.create_rectangle(200, 100, 150, 200, fill="blue")
-        self.w.create_line(100, 0, 200, 100)
-        self.w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
-        self.w.place(x=200, y=0, width=100, height=200)
+        self.w = Canvas(master, width=300, height=300)
+        self.w.create_rectangle(20, 20, 280, 280, fill="blue")
+        self.w.create_line(10, 10, 290, 10, fill="red")
+        self.w.create_line(290, 10, 290, 290, fill="red")
+        self.w.create_line(290, 290, 10, 290, fill="red")
+        self.w.create_line(10, 290, 10, 10, fill="red")
+        self.w.place(x=175, y=0, width=300, height=300)
 
+    def create_bar(self):
+        self.bar = Canvas(self, width=300, height=300)
     def start_background_task(self):
-        t = threading.Thread(target=begin_logging)
+        t = threading.Thread(target=self.begin_logging)
         t.start()
 
     def end_background_task(self):
-        threading.start_new_thread(stop_logging, ())
+        threading.start_new_thread(self.stop_logging, ())
 
-def begin_logging():
-    global logging
-    logging=True
-    filename = my_gui.fileref.get()
-    file = open((str(filename) + ".csv"), 'a+')
-    file.write("Angular Position \n x:, y:, z:\n")
-    portno = my_gui.entry.get()
-    ser = serial.Serial(('COM' + portno), 9600, timeout=1)  # open serial port
-    print(ser.name)         # check which port was really used
-    while logging==True:
-	    x = ser.read() 
-	    file.write(x)
-    ser.close()             # close port
+    def begin_logging(self):
+        global logging
+        logging=True
+        value = ''
+        filename = my_gui.fileref.get()
+        file = open((str(filename) + ".csv"), 'a+')
+        file.write("Angular Position \n x:, y:, z:\n")
+        portno = my_gui.entry.get()
+        ser = serial.Serial(('COM' + portno), 9600, timeout=1)  # open serial port
+        print(ser.name)         # check which port was really used
+        #bar_display = Canvas(self, width=280, height=280)
+        while logging==True:
+                x = ser.read()
+                if (x=='\n'):
+                    while x != ',':
+                        x = ser.read()
+                        value = value + x
+                    print (value)
 
-def stop_logging():
-    global logging
-    logging=False
+                        
+                        
+                    
+                file.write(x)
+        ser.close()             # close port
+
+    def stop_logging(self):
+        global logging
+        logging=False
 
 root = Tk()
-root.geometry("400x200+30+30") 
+root.geometry("475x300+30+30") 
 my_gui = MyFirstGUI(root)
 root.mainloop()
 
