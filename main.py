ba22e249b0d104ca86e91d08c5dea77ebf3917b2
Tristan <tristanb_44@hotmail.com>
2017-09-25 19:56:42 -0300

    added file read operations for playback function

--- main.py
+++ main.py
@@ -39,28 +39,35 @@ class MyFirstGUI:
         self.w.create_line(10, 280, 10, 10, fill="red")
         self.w.place(x=175, y=0, width=300, height=300)
 
-        self.start_button = Button(master, text="Log Data", command=lambda: self.start_background_task(self.w))
+        self.start_button = Button(master, text="Log Data", command=lambda: self.start_background_task1(self.w))
         self.start_button.place(x=0, y=96, width=80, height=20)
         
-        self.stop_button = Button(master, text="Stop Logging", command=self.stop_logging)
+        self.stop_button = Button(master, text="Stop", command=self.stop_logging)
         self.stop_button.place(x=81, y=96, width=80, height=20)
+
+        self.play_button = Button(master, text="Playback", command=lambda: self.start_background_task2(self.w))
+        self.play_button.place(x=0, y=116, width=80, height=20)
         
         self.close_button = Button(master, text="Close", command=self.close_application)
         self.close_button.grid(row=10, column=0, columnspan=1, sticky=S)
-        self.close_button.place(x=0, y=116, width=60, height=20)
+        self.close_button.place(x=0, y=136, width=60, height=20)
 
     def create_bar(self, argv):
         self.bar = Canvas(self, width=300, height=300)
         self.bar.create_line(150, 290, 150, argv, fill="red")
 
         
-    def start_background_task(self, canvas):
+    def start_background_task1(self, canvas):
         t = threading.Thread(target=self.begin_logging, args=[canvas])
+        t.start()
+        
+    def start_background_task2(self, canvas):
+        t = threading.Thread(target=self.playback, args=[canvas])
         t.start()
 
     def close_application(self):
         self.stop_logging()
-        my_gui.quit()
+        root.quit()
 
     def begin_logging(self, canvas):
         global logging
@@ -80,14 +87,6 @@ class MyFirstGUI:
         portno = self.entry.get()
         ser = serial.Serial(('COM' + portno), 19200, timeout=1)  # open serial port
         print(ser.name)         # check which port was really used
-        #while 1:
-            #canvas.create_line(200, 20, 200, i, fill="red",  tag="line")
-            #canvas.place(x=175, y=0, width=300, height=300)
-            #i=i+1
-            #time.sleep(0.02)
-            #if(i > 300):
-                #i=0
-                #canvas.delete("line")
         while logging==True:
             x = ser.read()
             if x=='x':
@@ -96,11 +95,12 @@ class MyFirstGUI:
                     if(x != '-'):
                         value <<= 8
                         value = value + ord(x)
-                    #print(ord(x))
                 print('x')    
-                print(value)
+                print(value-180)
+                file.write(str(value-180))
+                file.write(',')
                 canvas.delete("linex")
-                canvas.create_arc(20, 20, 90, 90, start=value, fill="red", tag="linex")
+                canvas.create_arc(20, 20, 90, 90, start=value, extent=20, fill="red", tag="linex")
                 value = 0
             elif x=='y':
                 while x != '-':
@@ -108,11 +108,12 @@ class MyFirstGUI:
                     if(x != '-'):
                         value <<= 8
                         value = value + ord(x)
-                    #print(ord(x))
                 print('y')
-                print(value)
+                print(value-180)
+                file.write(str(value-180))
+                file.write(',')
                 canvas.delete("liney")
-                canvas.create_arc(20, 110, 90, 180, start=value, fill="red", tag="liney")
+                canvas.create_arc(20, 110, 90, 180, start=value, extent=20, fill="red", tag="liney")
                 value = 0
             elif x=='z':
                 while x != '-':
@@ -120,52 +121,90 @@ class MyFirstGUI:
                     if(x != '-'):
                         value <<= 8
                         value = value + ord(x)
-                    #print(ord(x))
                 print('z')    
-                print(value)
+                print(value-180)
+                file.write(str(value-180))
+                file.write(',')
+                file.write('\n')
                 canvas.delete("linez")
-                canvas.create_arc(20, 200, 90, 270, start=value, fill="red", tag="linez")
+                canvas.create_arc(20, 200, 90, 270, start=value, extent=20, fill="red", tag="linez")
                 value = 0
-
-    def dummycode():
-        while logging==True:
-                time.sleep(0.01)
-                while x != ',':
-                    Xaxis = Xaxis + x
-                    x = ser.read()
-                    print(repr(x))
-                    file.write(x)
-                x=''
-                Xaxis = Xaxis.translate(None, '\n. ,-')
-                canvas.delete("linex")
-                canvas.create_arc(20, 20, 90, 90, start=(int(Xaxis)/1000), fill="red", tag="linex")
-                lastXaxis = int(Xaxis)
-                Xaxis = ''
-                while x != ',':
-                    Yaxis = Yaxis + x
-                    x = ser.read()
-                    print(repr(x))
-                    file.write(x)
-                x=''
-                Yaxis = Yaxis.translate(None, '\n. ,-')
-                canvas.delete("liney")
-                canvas.create_arc(20, 110, 90, 180, start=(int(Yaxis)/1000), fill="red", tag="liney")
-                lastYaxis = int(Yaxis)
-                Yaxis = ''
-                while x != '\n':
-                    Zaxis = Zaxis + x
-                    x = ser.read()
-                    print(repr(x))
-                    file.write(x)
-                x=''
-                Zaxis = Zaxis.translate(None, '\n. ,-')
-                canvas.delete("linez")
-                canvas.create_arc(20, 200, 90, 270, start=(int(Zaxis)/1000), fill="red", tag="linez")
-                lastZaxis = int(Zaxis)
-                Zaxis = ''
         ser.close()             # close port
-        file.close()
+        file.closed
 
+    def playback(self, canvas):
+        global logging
+        logging=True
+        sign=False
+        value = 0
+        filename = self.fileref.get()
+        file = open((str(filename) + ".csv"), 'r')
+        file.readline()
+        file.readline()
+        x = '0'
+        while ((logging==True) & (x != '')):
+            time.sleep(0.1)
+            while x != ',':
+                x = file.read(1)
+                if x == '':
+                    break
+                if (x == '\n'):
+                    break
+                if (x == '-'):
+                    sign=True
+                if ((x != ',') & (x != '-')):
+                    value = value*10
+                    value = value + (int(x)-48)
+            if (sign==True):
+                value = 0 - value
+            canvas.delete("linex")
+            canvas.create_arc(20, 20, 90, 90, start=value, extent=20, fill="red", tag="linex")
+            value = 0
+            sign=False
+            if(x==','):
+                x = '0'
+            while x != ',':
+                x = file.read(1)
+                if x == '':
+                    break
+                if (x == '\n'):
+                    break
+                if (x == '-'):
+                    sign=True
+                if ((x != ',') & (x != '-')):
+                    value = value*10
+                    value = value + (int(x)-48)
+            if (sign==True):
+                value = 0 - value
+            canvas.delete("liney")
+            canvas.create_arc(20, 110, 90, 180, start=value, extent=20, fill="red", tag="liney")
+            value = 0
+            sign=False
+            if(x==','):
+                x = '0'
+            while x != ',':
+                x = file.read(1)
+                if x == '':
+                    break
+                if (x == '\n'):
+                    break
+                if (x == '-'):
+                    sign=True
+                if ((x != ',') & (x != '-')):
+                    value = value*10
+                    value = value + (int(x)-48)
+            if (sign==True):
+                value = 0 - value
+            canvas.delete("linez")
+            canvas.create_arc(20, 200, 90, 270, start=value, extent=20, fill="red", tag="linez")
+            value = 0
+            sign=False
+            if(x==','):
+                x = '0'
+        file.closed
+        print("finished playback")
+
+        
     def stop_logging(self):
         global logging
         logging=False
